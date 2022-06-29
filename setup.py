93316c48090b2deb2c178e269500f45636c1540b
RDoerfel <doerfelruben@aol.com>
2022-06-29 09:58:14 +0200

    ENH: modify setup.py to make installation easier

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pyment',
       version='1.0',
@@ -6,4 +6,18 @@ setup(name='pyment',
       author='Esten H. Leonardsen',
       author_email='estenhl@psykologi.uio.no',
       url='https://github.com/estenhl/pyment-public',
-      packages=['pyment'])
+      packages=['pyment'],
+      python_requieres='3.9',
+      install_requires=[
+            'jupyterlab==3.2.1',
+            'matplotlib==3.4.3',
+            'mock==4.0.3',
+            'nibabel==3.2.1',
+            'pandas==1.3.4',
+            'pytest==6.2.4',
+            'requests==2.25.1',
+            'scikit-learn==0.24.2',
+            'tqdm==4.61.1',
+            'xlrd==2.0.1',
+            'tensorflow==2.9.1'
+      ])
